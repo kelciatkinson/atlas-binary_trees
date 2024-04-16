86f97f70aa29eb756f21684607ba67606ba64a84
“kelciatkinson” <kelciatkinson@gmail.com>
2024-04-16 09:50:36 -0500

    set up binary trees files including header & tests

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node- creates a binary tree node
+ *
+ * @parent: pointer to the parent node of the node to create
+ * @value: the value to put in the new node
+ *
+ * Return: pointer to the new node, or NULL if failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    if (*p == NULL)
+        return (NULL);
+}
