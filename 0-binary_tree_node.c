d407efea363e11de3f72de0602c9b134e1bf1d01
“kelciatkinson” <kelciatkinson@gmail.com>
2024-04-16 10:25:48 -0500

    started binary_tree_node function

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,6 +11,6 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    if (*p == NULL)
-        return (NULL);
+    new_node = malloc(sizeof(struct binary_tree_t));
+    new_node->left = binary
 }
