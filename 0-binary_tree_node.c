5b71e9de91c3b7d1c071c7d320a6e6256ee7c1ab
“kelciatkinson” <kelciatkinson@gmail.com>
2024-04-18 04:51:39 +0000

    created binary_tree_node function to addbinary tree nodes

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,6 +11,16 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    new_node = malloc(sizeof(struct binary_tree_t));
-    new_node->left = binary
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
 }
