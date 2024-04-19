1a0eeccb74ffbea7c6eaecdedf84dae6a47708ba
“kelciatkinson” <kelciatkinson@gmail.com>
2024-04-19 22:42:29 +0000

    fixed documentation

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves-
+ * binary_tree_leaves- counts the leaves in a binary tree
  *
  * @tree: pointer to the root node of the tree to count the number of leaves
  *
