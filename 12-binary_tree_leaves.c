295f4f2f25d28c42aaeed44ad9790607f6c8943c
“kelciatkinson” <kelciatkinson@gmail.com>
2024-04-19 22:42:55 +0000

    fixed spacing

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -18,7 +18,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (1);
 
-	leaves =  (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	leaves = (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 
 	return (leaves);
 }
