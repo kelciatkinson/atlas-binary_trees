4c4bebdd5f6d9aa9c46927d4b57a71809e631cfa
“kelciatkinson” <kelciatkinson@gmail.com>
2024-04-20 01:31:17 +0000

    added balance checking function and height helper function

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,56 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance- measures the balance factor of a binary tree
+ *
+ * @tree: pointer to the root nodd of the tree to measure the balance factor
+ *
+ * Return: the balance factor, or 0 if tree is NULL
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+	size_t balance = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+	
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	balance = left_height - right_height;
+
+	return (balance);
+}
+
+/**
+ * binary_tree_height-measures the height of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: 0 if tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height);
+
+	return (right_height);
+}
